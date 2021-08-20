5489352c067d5bcac6a76e7c0fabf7c9fc90ace2
llabuschagne <llabuschagne@alphawave.co.za>
2021-08-20 08:03:30 +0200

    notebook and app ready for presentation

---
+++ app/app-enhanced.py
@@ -0,0 +1,81 @@
+import dash
+import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+import dash_html_components as html
+import pandas as pd
+import plotly.graph_objects as go
+
+from dash.dependencies import Input, Output
+from pathlib import Path
+
+data_dir = Path('../notebooks/data')
+
+df_philosopher = pd.read_csv(data_dir/'Harry Potter 1.csv', sep=';')
+df_philosopher.columns = [c.lower().strip().replace(' ', '_')
+                          for c in df_philosopher.columns]
+df_philosopher['character'] = df_philosopher['character'].str.strip().str.lower()
+df_philosopher['sentence'] = df_philosopher['sentence'].str.lower()
+
+for character in df_philosopher['character'].unique():
+    df_philosopher[f'{character}'] = df_philosopher['sentence'].str.count(
+        character)
+
+df_mentions = df_philosopher.groupby('character')\
+    .sum().reset_index()\
+    .melt(id_vars='character', var_name='mentions', value_name='count')
+
+characters = list(df_mentions['character'].unique())
+
+df_mentions['speaker'] = df_mentions['character'].apply(
+    lambda ai: characters.index(ai))
+df_mentions['mentioner'] = df_mentions['mentions'].apply(
+    lambda ai: characters.index(ai)+len(character))
+
+
+dropdown = dcc.Dropdown(
+    options=[{'label': c, 'value': c} for c in characters],
+    value=characters[0],
+    multi=True,
+    id='selected-character'
+)
+
+app = dash.Dash(
+    external_stylesheets=[dbc.themes.BOOTSTRAP]
+)
+
+@app.callback(
+    Output(component_id='output-figure', component_property='figure'),
+    Input(component_id='selected-character', component_property='value')
+)
+def update_output_div(chars):
+    if type(chars) == str:
+        chars = [chars]
+
+    df = df_mentions[df_mentions['character'].isin(chars)]
+
+    fig = go.Figure(data=[go.Sankey(
+        node=dict(
+            label=characters+characters,
+        ),
+        link=dict(
+            source=df['speaker'].values,
+            target=df['mentioner'].values,
+            value=df['count'].values
+        ))])
+
+    fig.update_layout(title_text="Who speaks about who",
+                      height=1000,
+                      )
+
+    return fig
+
+
+app.layout = dbc.Container(html.Div([
+    dropdown,
+    dcc.Graph(figure=go.Figure(),
+              id='output-figure')
+
+
+]))
+if __name__ == "__main__":
+    app.run_server()
